987250a324826d037e858145c27fbb7f72b06bb8
Derik Ramirez <rderik@gmail.com>
2019-07-05 07:14:14 -0600

    Initial Commit

---
+++ ElTallerCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ElTallerCocoa
+//
+//  Created by Derik Ramirez on 7/5/19.
+//  Copyright © 2019 Derik Ramirez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
